2f2dfd24bde7f59484c2f3be7d57ed458a44ad2c
Eunice Ceniza <Eunice.Ceniza@dtn.com>
2022-12-02 11:40:32 +0800

    day30 project: error handling

--- project_day26/main.py
+++ project_day26/main.py
@@ -4,6 +4,13 @@ nato_df = pd.read_csv('nato_phonetic_alphabet.csv')
 nato_dict = {row[0]: row[1] for (_, row) in nato_df.iterrows()}
 
 
-word_input = input("Enter a word: ")
-output = [nato_dict[c.upper()] for c in word_input]
+while True:
+  word_input = input("Enter a word: ")
+  try:
+    output = [nato_dict[c.upper()] for c in word_input]
+  except KeyError:
+    print("Sorry, only letters in the alphabet please.")
+  else:
+    break
+
 print(output)
--- project_day29_pwmanager/main.py
+++ project_day29_pwmanager/main.py
@@ -1,3 +1,4 @@
+import json
 from tkinter import *
 from tkinter import messagebox
 
@@ -8,6 +9,29 @@ from project_day5_pwgenerator import generate
 LETTERS = 8
 NUMBERS = 4
 SYMBOLS = 4
+
+all_passwords = {}
+try:
+  with open('passwords.json') as f:
+    s = f.readlines()
+  all_passwords = json.JSONDecoder().decode(s)
+except:
+  pass
+
+
+def search_record():
+  website = website_input.get()
+
+  try:
+    password = all_passwords[website]
+  except KeyError:
+    if website in " \t\n\r" or not website:
+      message = 'Please enter a website.'
+    else:
+      message = f'No password saved for the website {website}.'
+    messagebox.showerror('Error', message)
+  else:
+    messagebox.showinfo(f'{website}', f"Username: {password['username']}\nPassword: {password['password']}")
 
 def generate_password():
   new_password = generate(LETTERS, SYMBOLS, NUMBERS)
@@ -25,12 +49,13 @@ def save_password():
     website_input.delete(0, END)
     username_input.delete(0, END)
     password_input.delete(0, END)
-    messagebox.showinfo(title='Information', message='Password has been saved.')
 
-    with open('passwords.txt', 'a') as f:
-      record = "{\n Website: " + website + "\n Username: " + username + "\n Password: " + password + "\n}\n"
+    all_passwords[website] = {'username': username, 'password': password}
+    with open('passwords.json', 'w') as f:
+      record = json.JSONEncoder(indent=2).encode(all_passwords)
       f.write(record)
 
+    messagebox.showinfo(title='Information', message='Password has been saved.')
     return
   
   messagebox.showerror(title='Error', message='Please fill in the required fields.')
@@ -45,19 +70,19 @@ image_label = Label(image=logo_png)
 website_label = Label(text='Website:', width=12, anchor='w')
 username_label = Label(text='Username/Email:', width=12, anchor='w')
 password_label = Label(text='Password:', width=12, anchor='w')
-website_input = Entry(width=40)
+website_input = Entry(width=22)
 username_input = Entry(width=40)
 password_input = Entry(width=25, font=('Monaco'))
+search_btn = Button(text='Search', width=13, command=search_record)
 generate_btn = Button(text='Generate Password', command=generate_password)
 add_btn = Button(text='Add', width=40, command=save_password)
 
 layout = {image_label: (0,1), website_label: (1,0), username_label: (2,0),
           password_label: (3,0), website_input: (1,1), username_input: (2,1),
-          password_input: (3,1), generate_btn: (3,2), add_btn: (4,0)}
+          password_input: (3,1), search_btn: (1,2), generate_btn: (3,2), add_btn: (4,0)}
 for widget in layout:
   widget.grid(row=layout[widget][0], column=layout[widget][1])
 
-website_input.grid(columnspan=2)
 username_input.grid(columnspan=2)
 password_input.grid(columnspan=1)
 generate_btn.grid(columnspan=1)
